c65a1d5afbcae677adca825478482514435a3b30
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2019-02-22 13:35:33 -0500

    Update music.py

--- music.py
+++ music.py
@@ -15,7 +15,7 @@ def query(x):
 	assert result.status_code == 200, "Error with connection to google"
 	soup = BeautifulSoup(result.content, features="html.parser")
 	results = soup.find_all("div", {"class": "g"})
-	p(results)
+	p(results[0])
 
 
 def clean(name):
