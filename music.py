27a8c5bcc8400b91b6b2d9c5fd8e8ddc290a3910
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2019-02-22 13:21:40 -0500

    Create music.py

---
+++ music.py
@@ -0,0 +1,29 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+sample = "https://www.google.com/search?q=site%3Awww.youtube.com+i+like+to+singa"
+template = "https://www.google.com/search?q=site%3Awww.youtube.com+%s"
+
+dll = []
+if sys.argv[1:]:
+	for x in sys.argv[1:]:
+		assert isinstance(x, str), "Invalid arguement type must be song name as string"
+		print("Searching for song: %s"%x)
+		dll += [query(clean(x))]
+
+def query(x):
+	'''Takes song name queries google and returns URL'''
+	result = requests.get(template)
+	assert result.status_code == 200, "Error with connection to google"
+	soup = BeautifulSoup(result.content)
+	result = soup.find_all("div", {"class": "g"})[0]
+	r = result.find_all(lambda tag: tag.name == 'a' and tag.has_attr('href'))
+	return r['href']
+
+def clean(name):
+	'''Transforms text name to query valid syntax'''
+	if ' ' in name:
+		\
+		name = name.replace(' ','+')
+	return name
